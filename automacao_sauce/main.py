66a67f862f687a1a9a16c562a1d8737752c44898
guiSanMelo <guilherme.ilhotas2004@hotmail.com>
2026-05-05 19:58:55 -0300

    Teste rodando no github actions

--- automacao_sauce/main.py
+++ automacao_sauce/main.py
@@ -28,7 +28,7 @@ class Main:
             orcamento=float(os.getenv("SAUCE_ORCAMENTO"))
         )
 
-        driver = self._actions.setUp(interface_ligada=True)
+        driver = self._actions.setUp()
         try:
             self._login.abrir_site(driver)
             self._login.credenciais_login(cliente, driver)
